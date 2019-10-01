6e318ecafbd8522d5e0612a37ba167791db4d0ba
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-09-30 21:37:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun(chill= "hangout with friends", d_fault = "roller-skate") {
+    `This Saturday, I want to ${chill} !`
+}
+
+function mondayWork(workHard= "goto the beach", revert = "go to the office") {
+    
+}
