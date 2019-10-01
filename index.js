ce907627bb1739741bcdeb4e460750e47983b288
George <georuiz817@gmail.com>
2019-10-01 01:57:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+function saturdayFun(thing="roller-skate"){
+return `This Saturday, I want to ${thing}!`
+}
+
+saturdayFun()
+saturdayFun("bathe my dog")
+
+let mondayWork = function(stuff = "go to the office") { 
+ return  `This Monday, I will ${stuff}.`
+}
+
+mondayWork()
+
+
+function wrapAdjective(flair = "*") {
+        return function innerScope(mood = "special") {
+           return `You are ${flair}${mood}${flair}!`
+        }
+     }
+
+     const Calculator = {
+        add: function(num1, num2){
+           return num1 + num2
+        },
+        
+        subtract: function(num1, num2) {
+          return  num1 - num2
+        },
+
+        multiply: function(num1, num2){
+          return  num1 * num2
+        },
+
+        divide: function(num1, num2) {
+        return num1 / num2
+        },
+     
+    }
+     
+    function actionApplyer(i,array){
+
+        
+
+    }
