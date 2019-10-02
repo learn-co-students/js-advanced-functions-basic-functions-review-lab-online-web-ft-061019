bd4a9034345331ec542c3693c6421739e69feb7c
Mic Fox <michael.c.fox.ii@gmail.com>
2019-10-01 20:48:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+   return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(f = "*"){
+    return function(adj = "special"){
+        return `You are ${f}${adj}${f}!`
+    } 
+}
+
+const Calculator = {
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+    multiply: (num1, num2) => num1 * num2, 
+    divide: (num1, num2) => num1 / num2
+}
+
+function actionApplyer(startingInteger, arryOfFnctns){
+    if (arryOfFnctns.length === 0) {
+        return startingInteger
+    } else {
+        let newInteger = startingInteger
+        arryOfFnctns.forEach(element => {
+            newInteger = element(newInteger);
+        });
+        return newInteger
+    }
+}
